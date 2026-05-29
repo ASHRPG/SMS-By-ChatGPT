dfa27da8196a834f925c9af5f3a9cf4ccbf996e9
ASHRPG <roshanprasadgu2006pta@gmail.com>
2026-05-29 18:42:25 +0530

    Added missing CityLookup.aspx.vb code-behind file

---
+++ Lookup/CityLookup.aspx.vb
@@ -0,0 +1,8 @@
+Partial Class Lookup_CityLookup
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
