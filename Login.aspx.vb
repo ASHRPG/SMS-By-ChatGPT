d85c97b1d7659584a36e976b324282750db09802
ASHRPG <roshanprasadgu2006pta@gmail.com>
2026-05-29 15:44:22 +0530

    Updated login authentication system with database integration

--- Login.aspx.vb
+++ Login.aspx.vb
@@ -1,12 +1,29 @@
+Imports System.Data
+
 Partial Class Login
     Inherits System.Web.UI.Page
 
+    Dim db As New DBManager()
+
     Protected Sub btnLogin_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnLogin.Click
-        If txtUsername.Text = "admin" And txtPassword.Text = "admin123" Then
-            Session("User") = txtUsername.Text
-            Response.Redirect("Dashboard.aspx")
-        Else
-            Response.Write("<script>alert('Invalid Username or Password')</script>")
-        End If
+
+        Try
+            Dim query As String
+            query = "SELECT * FROM Users WHERE Username='" & txtUsername.Text & "' AND Password='" & txtPassword.Text & "'"
+
+            Dim dt As DataTable = db.GetData(query)
+
+            If dt.Rows.Count > 0 Then
+                Session("Username") = txtUsername.Text
+                Response.Redirect("~/Dashboard/Analytics.aspx")
+            Else
+                lblMessage.Text = "Invalid Username or Password"
+            End If
+
+        Catch ex As Exception
+            lblMessage.Text = ex.Message
+        End Try
+
     End Sub
+
 End Class
