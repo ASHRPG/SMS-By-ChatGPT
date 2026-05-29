0aa61122d7f5c139ec8aebfce4d70f8ac469b57c
ASHRPG <roshanprasadgu2006pta@gmail.com>
2026-05-29 15:40:35 +0530

    Updated dashboard analytics code-behind with ERP statistics and error handling

--- Dashboard/Analytics.aspx.vb
+++ Dashboard/Analytics.aspx.vb
@@ -7,8 +7,19 @@ Partial Class Dashboard_Analytics
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
         If Not IsPostBack Then
-            lblStudents.Text = db.GetData("SELECT * FROM Student").Rows.Count.ToString()
-            lblFees.Text = db.GetData("SELECT * FROM Fees").Rows.Count.ToString()
+            LoadDashboard()
         End If
     End Sub
+
+    Private Sub LoadDashboard()
+        Try
+            lblStudents.Text = db.GetData("SELECT COUNT(*) FROM Student").Rows(0)(0).ToString()
+            lblFees.Text = db.GetData("SELECT COUNT(*) FROM Fees").Rows(0)(0).ToString()
+            lblStaff.Text = db.GetData("SELECT COUNT(*) FROM Staff").Rows(0)(0).ToString()
+            lblBooks.Text = db.GetData("SELECT COUNT(*) FROM LibraryBooks").Rows(0)(0).ToString()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
 End Class
